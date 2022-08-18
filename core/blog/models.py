74444dd6ef4b1d77ba9ec470e6a835ea24aebcdc
Magic-Mario <magic.mario2021@gmail.com>
2022-08-18 12:27:48 -0500

    Initial commit with blog app

---
+++ core/blog/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/blog/<title>/<filename>
+    return 'blog/{instance.title}/{filename}'
+
+
+class Post(models.Model):
+
+    class PostObjects(models.Manager):
+        # Filter the post by status
+        def get_queryset(self):
+            return super().get_queryset().filter(status='published')
+
+    options = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=255)
+    thumbnail = models.ImageField(
+        upload_to=user_directory_path, default='default.jpg', blank=True, null=True)
+    excerpt = models.TextField(null=True)
+    content = models.TextField()
+    slug = models.SlugField(
+        max_length=255, unique_for_date='published', null=False, unique=True)
+    published = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='post_user')
+
+    # Give the option to publish or draft the post
+    status = models.CharField(max_length=10, choices=options, default='draft')
+    objects = models.Manager()
+    postobjects = PostObjects()
+
+    # Ordering the posts by published date
+    class Meta:
+        ordering = ('-published',)
+        
+    def __str__(self):
+        return self.title
